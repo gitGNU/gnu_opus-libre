9658f898dc206c3964e4ac95fe3fa39537cf8df0
Valentin Villenave <valentin@villenave.net>
2012-11-20 11:02:43 +0000

    New feature: allow for score-specific layout blocks
    This feature is limited for now: one has to prefix
    the \layout{} block with $$defaultlayout in order
    not to lose any previous \layout settings.

--- lib/80-buildskel.scm
+++ lib/80-buildskel.scm
@@ -51,6 +51,17 @@ markup exists."
               (regexp-substitute/global #f "[A-Z]" name 'pre " "0 'post)
               (make-null-markup))))))
 
+(define (make-this-layout name suffix)
+  "Associate NAME with SUFFIX, and check if a local \\layout{} block
+exists with that name.  If so, parse it."
+  (let* ((fullname (string-append name suffix))
+         (def (ly:parser-lookup parser (string->symbol fullname))))
+    (if (ly:output-def? def)
+        (begin (ly:debug-message "Using layout definition from variable ~a" fullname)
+               def)
+        (begin (ly:debug-message "No layout definitions stored in ~a" fullname)
+               #f ))))
+
 (define newVoice
   ;;   "If NAME matches a defined music expression, then
   ;; create a Voice for it.  If a matching timeline can be
